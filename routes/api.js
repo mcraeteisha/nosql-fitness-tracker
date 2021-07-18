1eaeec578cb8a1b3cccb08352bca3dbb9e367613
mcraet <mcraeteisha@gmail.com>
2021-07-18 16:37:53 -0400

    added api routes

---
+++ routes/api.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+//Get route for workouts
+router.get("/api/workouts", (req, res) => {
+    Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  });
+
+
+//Post route to create workout
+router.post("/api/workouts", ({body}, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+
+
+module.exports = router;
